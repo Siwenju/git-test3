b936326180aa3c9539d122b9ce23add3f58be7f3
Si <1542365350@qq.com>
2018-11-05 20:10:48 +0800

    提交ahaha

---
+++ ahaha/src/cn/itcast/ahaaa.java
@@ -0,0 +1,9 @@
+package cn.itcast;
+
+public class ahaaa {
+    public static void main(String[] args) {
+        System.out.println("suussuus");
+        System.out.println("suussuus");
+        System.out.println("suussuus");
+    }
+}
